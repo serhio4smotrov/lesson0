33af264e40a024a320edb68fb1554f83f9e13da9
serhio4smotrov <ser19061995@gmail.com>
2024-08-27 21:24:28 +0300

    variable

---
+++ variable.py
@@ -0,0 +1,5 @@
+number_of_tasks = 12
+number_of_hours = 1.5
+name_of_class = 'Python'
+time_one_task = number_of_hours / number_of_tasks
+print('Курс:',name_of_class,', всего задач:', number_of_tasks,', затрачено часов:', number_of_hours, ', среднее время выполнения', time_one_task, 'часа')
