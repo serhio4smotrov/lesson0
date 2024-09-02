52f49a0936d134013b4a577ca661420d7d0709b2
serhio4smotrov <ser19061995@gmail.com>
2024-09-02 12:51:34 +0300

    initial

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input('Введите строку: ')
+print('Количество символов строки: ',len(my_string))
+print('Строка в верхнем регистре:',my_string.upper())
+print('Строка в нижнем регистре: ',my_string.lower())
+print('Строка без пробелов: ',my_string.replace(' ',''))
+print('Первый символ строки: ',my_string[0])
+print('Последний символ строки: ',my_string[-1])
