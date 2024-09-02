53298ecb50e6c0f951dc7ab0bee928dc88982205
serhio4smotrov <ser19061995@gmail.com>
2024-09-02 14:09:05 +0300

    initial

---
+++ module_1_5.py
@@ -0,0 +1,6 @@
+immutable_var = 2,6,'v',True,5.4
+print(immutable_var)
+# immutable[0] = 2 кортеж не поддерживает изменения своих элементов
+mutable_list = [4.5,7,'w',False]
+mutable_list[1] = 9
+print(mutable_list)
